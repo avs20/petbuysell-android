8c8a3931ea10941f70cc5d7243aae6f53f38c0f9
Ashutosh Singh <dh_ashu@outlook.com>
2015-01-12 22:00:09 +0530

    Adding the layout file for the single Ad result.

--- app/src/main/java/com/siolabs/myapplication/com/siolabs/myapplication/adapters/DemoObjectFragment.java
+++ app/src/main/java/com/siolabs/myapplication/com/siolabs/myapplication/adapters/DemoObjectFragment.java
@@ -12,18 +12,31 @@ import com.siolabs.myapplication.R;
 // Instances of this class are fragments representing a single
 // object in our collection.
     public  class DemoObjectFragment extends Fragment {
-        public static final String ARG_OBJECT = "object";
+    
+        public static final String ARG_OBJECT= "INDEX";
+        public  final String[] prices = getResources().getStringArray(R.array.dog_prices);
+        public  final String[] titles = getResources().getStringArray(R.array.dog_title);
+        public  final String[] desc = getResources().getStringArray(R.array.dog_desc);
+        public  final String[]  loca = getResources().getStringArray(R.array.dog_locations);
+        public final String[] users = getResources().getStringArray(R.array.user_name);
+    
 
+    
         @Override
         public View onCreateView(LayoutInflater inflater,
                                  ViewGroup container, Bundle savedInstanceState) {
             // The last two arguments ensure LayoutParams are inflated
             // properly.
+            
+           
             View rootView = inflater.inflate(
                     R.layout.single_ad_layout, container, false);
             Bundle args = getArguments();
-            ((TextView) rootView.findViewById(R.id.text1)).setText(
-                    Integer.toString(args.getInt(ARG_OBJECT)));
+            ((TextView) rootView.findViewById(R.id.priceTextView)).setText(prices[(args.getInt(ARG_OBJECT))]);
+            ((TextView) rootView.findViewById(R.id.titleTextview)).setText(titles[(args.getInt(ARG_OBJECT))]);
+            ((TextView) rootView.findViewById(R.id.descTextView)).setText(desc[(args.getInt(ARG_OBJECT))]);
+            ((TextView) rootView.findViewById(R.id.locationTextView)).setText(loca[(args.getInt(ARG_OBJECT))]);
+            ((TextView) rootView.findViewById(R.id.userTextView)).setText(users[(args.getInt(ARG_OBJECT))]);
             return rootView;
         }
     }
